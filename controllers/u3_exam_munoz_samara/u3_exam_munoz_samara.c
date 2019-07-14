02cd3c1cb966f66c62c86c1fce8f5a55b108416f
Samara <al1609086@upy.edu.mx>
2019-07-14 13:33:10 -0500

    no rear sensor needed

--- controllers/u3_exam_munoz_samara/u3_exam_munoz_samara.c
+++ controllers/u3_exam_munoz_samara/u3_exam_munoz_samara.c
@@ -10,36 +10,69 @@
  * You may need to add include files like <webots/distance_sensor.h> or
  * <webots/differential_wheels.h>, etc.
  */
+
+///////LIBRERIAS//////////////
 #include <webots/robot.h>
+#include <webots/robot.h>
+#include <webots/motor.h>
+#include <webots/position_sensor.h>
+#include <webots/distance_sensor.h>
+#include <webots/keyboard.h>
 
-/*
- * You may want to add macros here.
- */
+//////LIBRERIA STANDARD//////////
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//////////MACROS////////////////
 #define TIME_STEP 64
+#define PI 3.1416
 
-/*
- * This is the main program.
- * The arguments of the main function can be specified by the
- * "controllerArgs" field of the Robot node
- */
+
+int velocity=3;
+
 int main(int argc, char **argv)
 {
   /* necessary to initialize webots stuff */
   wb_robot_init();
+   
+  WbDeviceTag wheel_1 = wb_robot_get_device("wheel1");
+  WbDeviceTag wheel_2 = wb_robot_get_device("wheel2");
+  WbDeviceTag wheel_3 = wb_robot_get_device("wheel3");
+   
+  WbDeviceTag disleft = wb_robot_get_device("ds_left");
+  WbDeviceTag disright = wb_robot_get_device("ds_right");
+  WbDeviceTag disrear = wb_robot_get_device("ds_rear");
+   
+  WbDeviceTag pos1 = wb_robot_get_device("ps_1");
+  WbDeviceTag pos2 = wb_robot_get_device("ps_2");
+  WbDeviceTag pos3 = wb_robot_get_device("ps_3");
+  
+  
+  wb_distance_sensor_enable(disleft,TIME_STEP);
+  wb_distance_sensor_enable(disright,TIME_STEP);
+  wb_distance_sensor_enable(disrear,TIME_STEP);
+   
+     
+  wb_position_sensor_enable(pos1,TIME_STEP);
+  wb_position_sensor_enable(pos2,TIME_STEP);
+  wb_position_sensor_enable(pos3,TIME_STEP);
 
-  /*
-   * You should declare here WbDeviceTag variables for storing
-   * robot devices like this:
-   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
-   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
-   */
+  wb_motor_set_position(wheel_1,INFINITY);
+  wb_motor_set_position(wheel_2,INFINITY);
+  wb_motor_set_position(wheel_3,INFINITY);
+  
+ 
 
-  /* main loop
-   * Perform simulation steps of TIME_STEP milliseconds
-   * and leave the loop when the simulation is over
-   */
+  
+  
   while (wb_robot_step(TIME_STEP) != -1) {
 
+  wb_motor_set_velocity(wheel_1,-velocity);
+  wb_motor_set_velocity(wheel_2,velocity);
+  wb_motor_set_velocity(wheel_3,0); 
+    
+    
     /*
      * Read the sensors :
      * Enter here functions to read sensor data, like:
