aab38ab65f4a380f6681de2e10e0a4c358279a1a
Samara <al1609086@upy.edu.mx>
2019-07-16 00:35:31 -0500

    Code re-established, now trying ang and lin velocity

--- controllers/u3_exam_munoz_samara/u3_exam_munoz_samara.c
+++ controllers/u3_exam_munoz_samara/u3_exam_munoz_samara.c
@@ -27,33 +27,51 @@
 //////////MACROS////////////////
 #define TIME_STEP 64
 #define PI 3.1416
+#define OBSTACLE_DISTANCE 30.0
 
 
-int velocity=3;
+int key;
+
+double distance_left = 0;
+double distance_right = 0;
+double position = 0;
+int turn_left = 0;
+int turn_right = 0;
+double compare;
+int a = 1;
+int m = 0;
+int c=0;
+float angularVel, linealVel,RPM;
+  
+  
+
+
 
 int main(int argc, char **argv)
 {
   /* necessary to initialize webots stuff */
   wb_robot_init();
-   
+  wb_keyboard_enable(TIME_STEP);
+
+
   WbDeviceTag wheel_1 = wb_robot_get_device("wheel1");
   WbDeviceTag wheel_2 = wb_robot_get_device("wheel2");
   WbDeviceTag wheel_3 = wb_robot_get_device("wheel3");
-   
+
   WbDeviceTag disleft = wb_robot_get_device("ds_left");
   WbDeviceTag disright = wb_robot_get_device("ds_right");
-  WbDeviceTag disrear = wb_robot_get_device("ds_rear");
-   
+
+
   WbDeviceTag pos1 = wb_robot_get_device("ps_1");
   WbDeviceTag pos2 = wb_robot_get_device("ps_2");
   WbDeviceTag pos3 = wb_robot_get_device("ps_3");
-  
-  
+
+
   wb_distance_sensor_enable(disleft,TIME_STEP);
   wb_distance_sensor_enable(disright,TIME_STEP);
-  wb_distance_sensor_enable(disrear,TIME_STEP);
-   
-     
+
+
+
   wb_position_sensor_enable(pos1,TIME_STEP);
   wb_position_sensor_enable(pos2,TIME_STEP);
   wb_position_sensor_enable(pos3,TIME_STEP);
@@ -62,29 +80,203 @@ int main(int argc, char **argv)
   wb_motor_set_position(wheel_2,INFINITY);
   wb_motor_set_position(wheel_3,INFINITY);
   
- 
-
+  
+  printf(" 'Up Arrow' to move forward linearly at 0.3 m/s\n");
+  printf(" 'Down Arrow' to move backward linearly at 0.3 m/s\n");
+  printf(" 'Left Arrow' to move linearly to the left at 0.3 m/s\n");
+  printf(" 'Right Arrow'to move linearly to the right at 0.3 m/s\n");
+  printf(" 'A' key to turn the robot left at 45 degrees/s\n");
+  printf(" 'S' key to turn the robot right at 45 degrees/s\n");
+  printf(" 'G' key to start Autonomous Mode for the robot\n");
+  printf(" 'W' key to start Manual Mode\n");
+  fflush(stdout);
   
   
-  while (wb_robot_step(TIME_STEP) != -1) {
+  void Manual(){
 
-  wb_motor_set_velocity(wheel_1,-velocity);
-  wb_motor_set_velocity(wheel_2,velocity);
-  wb_motor_set_velocity(wheel_3,0); 
+    //Read Distance Sensor//
+    distance_left = wb_distance_sensor_get_value(disleft);
+    distance_right = wb_distance_sensor_get_value(disright);
+
+
+    //Read Position Sensor//
+    position = wb_position_sensor_get_value(pos1);
+
+    //Print Read Values//
+
+     printf("distance_left: %lf\r\n", distance_left);
+     printf("distance_right: %lf\r\n", distance_right);
+     printf("Encoder Postion: %lf\r\n", position);
+
+      wb_motor_set_velocity(wheel_1, 0);
+      wb_motor_set_velocity(wheel_2, 0);
+      wb_motor_set_velocity(wheel_3, 0);
+    
+   
+
+    if(key==WB_KEYBOARD_UP){
+
+      wb_motor_set_position(wheel_1, INFINITY);
+      wb_motor_set_velocity(wheel_1, 0);
+      wb_motor_set_position(wheel_2, INFINITY);
+      wb_motor_set_velocity(wheel_2, 0);
+      wb_motor_set_position(wheel_3, INFINITY);
+      wb_motor_set_velocity(wheel_3, 0);
+      }
+
+    if(key==WB_KEYBOARD_DOWN){
+
+      wb_motor_set_position(wheel_1, INFINITY);
+      wb_motor_set_velocity(wheel_1, 0);
+      wb_motor_set_position(wheel_2, INFINITY);
+      wb_motor_set_velocity(wheel_2, 0);
+      wb_motor_set_position(wheel_3, INFINITY);
+      wb_motor_set_velocity(wheel_3, 0);
+      }
+
+      if(key==WB_KEYBOARD_LEFT){
+
+      wb_motor_set_position(wheel_1, INFINITY);
+      wb_motor_set_velocity(wheel_1, -3);
+      wb_motor_set_position(wheel_2, INFINITY);
+      wb_motor_set_velocity(wheel_2, -3);
+      wb_motor_set_position(wheel_3, INFINITY);
+      wb_motor_set_velocity(wheel_3, 3*1.9);
+      }
+
+      if(key==WB_KEYBOARD_RIGHT){
+
+      wb_motor_set_position(wheel_1, INFINITY);
+      wb_motor_set_velocity(wheel_1, 3);
+      wb_motor_set_position(wheel_2, INFINITY);
+      wb_motor_set_velocity(wheel_2, 3);
+      wb_motor_set_position(wheel_3, INFINITY);
+      wb_motor_set_velocity(wheel_3, -3*1.9);
+      }
+
+      if(key == 'S' ){
+      compare = position + 0.785398; //0.7853... = 45 degrees to the left
+      turn_left = 1;
+      }
+
+      if(turn_left == 1){
+
+          if(position <= compare){
+            wb_motor_set_velocity(wheel_1, 5);
+            wb_motor_set_velocity(wheel_2, 5);
+            wb_motor_set_velocity(wheel_3, 5);
+            }
+
+          else{
+            wb_motor_set_velocity(wheel_1, 0);
+            wb_motor_set_velocity(wheel_2, 0);
+            wb_motor_set_velocity(wheel_3, 0);
+            turn_left = 0;
+            }
+      }
+
+      
+      if(key == 'A' ){
+      compare = position - 0.785398; //0.7853... = 45 degrees to the left
+      turn_right = 1;
+      }
+
+        if(turn_right == 1){
+  
+            if(position <= compare){
+              wb_motor_set_velocity(wheel_1, -5);
+              wb_motor_set_velocity(wheel_2, -5);
+              wb_motor_set_velocity(wheel_3, -5);
+              }
+  
+            else{
+              wb_motor_set_velocity(wheel_1, 0);
+              wb_motor_set_velocity(wheel_2, 0);
+              wb_motor_set_velocity(wheel_3, 0);
+              turn_right = 0;
+              }
+         }
+       }
+       
+   
+   
+   
+   void Automatic(){
+       
+          //Read Distance Sensor//
+      distance_left = wb_distance_sensor_get_value(disleft);
+      distance_right = wb_distance_sensor_get_value(disright);
+  
+  
+      //Read Position Sensor//
+      position = wb_position_sensor_get_value(pos1);
+      compare = position-c ;
+      c = position;
+      
+      
+      linealVel= ((compare/0.064)*0.06);
+      angularVel= (compare/0.064);
+      RPM =(compare/0.064)*(60/(2*3.1416));
+  
+      
+  
+      //Print Read Values//
+  
+       printf("distance_left: %lf\r\n", distance_left);
+       printf("distance_right: %lf\r\n", distance_right);
+       printf("Encoder1: %lf\r\n", position);
+       printf("Linear Velocity: %f\r\n", linealVel);
+       printf("Angular Velocity: %f\r\n", angularVel);
+       printf("RPM: %f\r\n", RPM);
+       
+       
+    
+       wb_motor_set_velocity(wheel_1, -6.66);
+       wb_motor_set_velocity(wheel_2, 6.66);
+       wb_motor_set_velocity(wheel_3, 0);
+
+       if(distance_left < distance_right && distance_left < OBSTACLE_DISTANCE){
+
+        wb_motor_set_velocity(wheel_1, -6.66);
+        wb_motor_set_velocity(wheel_2, -6.66);
+        wb_motor_set_velocity(wheel_3, -6.66);
+    
+       }
+       else if(distance_left > distance_right && distance_right < OBSTACLE_DISTANCE){
+    
+        wb_motor_set_velocity(wheel_1, 6.66);
+        wb_motor_set_velocity(wheel_2, 6.66);
+        wb_motor_set_velocity(wheel_3, 6.66);
+      }
+    
+    }
+
+    
+      
+while (wb_robot_step(TIME_STEP) != -1) {
+     key = wb_keyboard_get_key();
+
+      if(key == 'W'){
+        m = 1;
+        a = 0;
+        printf("Manual mode \n");
+      }
     
     
-    /*
-     * Read the sensors :
-     * Enter here functions to read sensor data, like:
-     *  double val = wb_distance_sensor_get_value(my_sensor);
-     */
-
-    /* Process sensor data here */
-
-    /*
-     * Enter here functions to send actuator commands, like:
-     * wb_differential_wheels_set_speed(100.0,100.0);
-     */
+      else if (key == 'G'){
+        a = 1;
+        m = 0;
+       printf("Automatic mode \n");
+    
+      }
+    
+      if(m == 1){
+      Manual();
+      }
+      if(a == 1){
+      Automatic();
+      }
+    
   };
 
   /* Enter your cleanup code here */
